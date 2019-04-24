ed15bf3e7ffcab13a13655639a7926e54cd8c0e4
Aliaksandr Rastarhuyeu <a.rastarhuyeu@unlitsolutions.com>
2019-04-24 15:53:48 +0300

    Add navigation

---
+++ Weather/WeatherApp/Page1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_OnClicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new Page1());
+		}
+	}
+}
